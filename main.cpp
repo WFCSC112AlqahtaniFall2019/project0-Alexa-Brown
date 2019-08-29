250bfd313264dbd7036f337582f433b4dfd1dee6
Alexa-Brown <browam18@wfu.edu>
2019-08-29 17:32:28 -0400

    began to answer the problem
    Signed-off-by: Alexa-Brown <browam18@wfu.edu>

--- main.cpp
+++ main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
 using namespace std;
-
+//Not finished yet
 int main() {
     cout << "Hello, World! " << endl;
+    int i=-10;
+    int j=-10;
+    int firstEquationx;
+    int firstEquationy;
+    int equalto1;
+    int equalto2;
+    int secondEquationx;
+    int secondEquationy;
+
+    cin>> firstEquationx;
+    cin>> firstEquationy;
+    cin>> equalto1;
+    cin>> secondEquationx;
+    cin>> secondEquationy;
+    cin>> equalto2;
+
+
+    for (i = -10; i >-11; i--){
+        cout << "Hi" << endl;
+        for (j = -10; j > -11; j--){
+            cout << "Hiii" << endl;
+            if ((firstEquationx * i) + (firstEquationy * i) == equalto1){
+                cout << "Hello"<<endl;
+                if ((secondEquationx * j) + (secondEquationy * j) == equalto2){
+                    cout << i << j << endl;
+                    break;
+                }
+            }
+        }
+    }
+        cout << "No solution." <<endl;
+
     return 0;
 }
