70254a15c3d78dc82bd8c604b78aeefd1069cb94
Alexa-Brown <browam18@wfu.edu>
2019-09-03 18:16:58 -0400

    continued to answer the problem
    Signed-off-by: Alexa-Brown <browam18@wfu.edu>

--- main.cpp
+++ main.cpp
@@ -12,11 +12,17 @@ int main() {
     int secondEquationx;
     int secondEquationy;
 
+    cout << "Enter the first x ";
     cin>> firstEquationx;
+    cout << "Enter the first y ";
     cin>> firstEquationy;
+    cout << "Enter the number the first equation is equal to ";
     cin>> equalto1;
+    cout << "Enter the second x ";
     cin>> secondEquationx;
+    cout << "Enter the second y ";
     cin>> secondEquationy;
+    cout << "Enter the number the second equation is equal to ";
     cin>> equalto2;
 
 
@@ -27,7 +33,7 @@ int main() {
             if ((firstEquationx * i) + (firstEquationy * i) == equalto1){
                 cout << "Hello"<<endl;
                 if ((secondEquationx * j) + (secondEquationy * j) == equalto2){
-                    cout << i << j << endl;
+                    cout << "x = " << i << "y = " << j << endl;
                     break;
                 }
             }
